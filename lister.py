d2262f78d8075fd4cfb594257bd37e01a7d78c24
paskma <paskma@a15def3e-3e5f-11de-b232-599c1b3c59e8>
2007-08-14 07:46:21 +0000

    Exception handler

--- lister.py
+++ lister.py
@@ -1,16 +1,19 @@
 import os, stat, types
 
 def walktree(topdir):
-	names = os.listdir(topdir)
-	for name in names:
-		full = os.path.join(topdir, name)
-		yield full
-		
-		st = os.lstat(full)
-		if stat.S_ISDIR(st.st_mode):
-			print "Diving into", full
-			for i in walktree(full):
-				yield i
+	try:
+		names = os.listdir(topdir)
+		for name in names:
+			full = os.path.join(topdir, name)
+			yield full
+			
+			st = os.lstat(full)
+			if stat.S_ISDIR(st.st_mode):
+				print "Diving into", full
+				for i in walktree(full):
+					yield i
+	except OSError, er:
+		print "warn", er
 
 def fulltree(topdir):
 	return [i for i in walktree(topdir)]
